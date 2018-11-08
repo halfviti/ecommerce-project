4b7feadf128c7e6809e84cf71f34ccd245637316
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-08 11:46:17 -0600

    Minor fixes (routing and docker port)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get ':permalink', to: 'pages#permalink'
-  root to: 'pages#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+  get ':permalink', to: 'pages#permalink'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
