0a2a628c7ed80c90197c753f6aa49ac88503c3fc
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-07 21:36:33 -0600

    Added image to products.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Product do
-  permit_params :name, :category_id, :description, :price
+  permit_params :name, :category_id, :description, :price, :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_181724) do
+ActiveRecord::Schema.define(version: 2018_11_08_033028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_181724) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
