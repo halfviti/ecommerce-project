038f2ce3fe09a834d7fa8e24c4563e1344df198c
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-10 15:40:03 -0600

    Added products controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  resources :products, only: [:index, :show]
+
   get ':permalink', to: 'pages#permalink'
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
