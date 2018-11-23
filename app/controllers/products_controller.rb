532a44233c559001101c9a8c58e3a69d8aabefc5
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-23 10:09:16 -0600

    Added breadcrumbs to products. :bread:

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
+  
   def index
+    breadcrumb 'Products', :products_path
     @products = Product.all.page params[:page]
   end
 
   def show
     @product = Product.find(params[:id])
+    breadcrumb @product.category.name, category_path(@product.category_id)
+    breadcrumb @product.name, product_path(params[:id])
   end
 end
