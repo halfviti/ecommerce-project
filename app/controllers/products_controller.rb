8dd5728412fac5f8ca531f8e5d1cf1c322ee4104
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-23 10:50:27 -0600

    Added products filter. :mag_right:

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,21 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
-  
   def index
     breadcrumb 'Products', :products_path
-    @products = Product.all.page params[:page]
+    if params[:filter].present?
+      if params[:filter] == 'new'
+        @products = Product.where('created_at > ?', 7.days.ago)
+                           .order(created_at: :desc)
+      end
+      if params[:filter] == 'updated'
+        @products = Product.where('updated_at > ?', 7.days.ago)
+                           .order(updated_at: :desc)
+      end
+    else
+      @products = Product.all
+    end
+    @products = @products&.page params[:page]
   end
 
   def show
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ProductsHelper
+  def filter_options
+    filters = []
+    filters << ['No Filter', '']
+    filters << ['New Products', 'new']
+    filters << ['Recently Updated', 'updated']
+    filters
+  end
 end
