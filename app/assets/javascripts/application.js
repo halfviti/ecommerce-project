f67f771155fdb2785b6d10e3618fad6e0d1d883a
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-27 09:34:14 -0600

    Prettied up flash messages. :tada:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+
+
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#notice_wrapper').fadeOut("slow", function() {
+            $(this).remove();
+        })
+    }, 1000)
+});
