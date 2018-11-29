fdd984ea37279fd238a380a8a86127d6a634693e
Gavin Johnson <g.ks.johnson@gmail.com>
2018-11-29 14:52:59 -0600

    Changed how flash messages are handled.  :love_letter:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   breadcrumb 'Home', :root_path
   before_action :set_layout_variables
   before_action :initialize_cart
-  add_flash_types :added, :updated_quantity, :removed_product
+  add_flash_types :success, :danger
 
   def set_layout_variables
     @categories = Category.all.order(:name)
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,13 +3,10 @@
 class CartController < ApplicationController
   before_action :define_quantity, only: %i[add_product update_quantity]
   before_action :get_product, only: %i[add_product remove_product update_quantity]
-  before_action :set_return, only: %i[add_product remove_product update_quantity]
+  before_action :set_return, only: %i[add_product remove_product update_quantity clear]
 
   def index
-    @cart = session[:cart].map do |x|
-      { product: Product.find(x['product_id']),
-        quantity: x['quantity'] }
-    end
+    @cart = helpers.read_cart
   end
 
   def add_product
@@ -19,20 +16,26 @@ class CartController < ApplicationController
       session[:cart].find { |x| x['product_id'] == @product.id }['quantity'] += @quantity
     end
     redirect_to session.delete(:return_to),
-                added: "#{@product.name} has been added to Cart."
+                success: "#{@product.name} has been added to Cart."
   end
 
   def remove_product
     session[:cart].delete_if { |x| x['product_id'] == @product.id }
     redirect_to session.delete(:return_to),
-                removed_product: "#{@product.name} removed from Cart."
+                danger: "#{@product.name} removed from Cart."
+  end
+
+  def clear
+    session[:cart] = []
+    redirect_to session.delete(:return_to),
+                danger: 'Your cart was emptied.'
   end
 
   def update_quantity
     session[:return_to] ||= request.referer
     session[:cart].find { |x| x['product_id'] == @product.id }['quantity'] = @quantity
     redirect_to session.delete(:return_to),
-                updated_quantity: "Updated product quantity: #{@product.name}"
+                success: "Updated product quantity: #{@product.name}"
   end
 
   def get_product
